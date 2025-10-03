3235cbc1bd6f5ec49d9594bea795a2b83a2efcb0
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-03 10:09:55 +0530

    .

--- MinorProject/ColorMixer/src/App.jsx
+++ MinorProject/ColorMixer/src/App.jsx
@@ -9,13 +9,41 @@ function App() {
   const [green,setGreen] = useState(0);
   const [blue,setBlue] = useState(0);
 
-  const handleRed =(e)={
-    
+  const handleRed =(value)=>{
+    setRed(value)
+  }
+  const handleGreen =(value)=>{
+    setGreen(value)
+  }
+  const handleBlue =(value)=>{
+    setBlue(value)
   }
 
   return (
     <>
-      
+      <div style={{
+        backgroundColor:'rgb(0,255,0)',
+        height:'500px',
+        width:'500px'
+      }} className="container">
+
+      </div>
+      <form action="">
+        <table>
+          <tr>
+            <td><label htmlFor="red">Red : </label></td>
+            <td><input type="range" id='red' defaultValue={0} min={0} max={255}/></td>
+          </tr>
+          <tr>
+            <td><label htmlFor="green">Green : </label></td>
+            <td><input type="range" id='green' defaultValue={0} min={0} max={255}/></td>
+          </tr>
+          <tr>
+            <td><label htmlFor="blue">Blue : </label></td>
+            <td><input type="range" id='blue' defaultValue={0} min={0} max={255}/></td>
+          </tr>
+        </table>
+      </form>
     </>
   )
 }
