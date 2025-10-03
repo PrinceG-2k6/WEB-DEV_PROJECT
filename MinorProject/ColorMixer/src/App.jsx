6933aa9f3ebd493530e202531fb52bbdcf963b87
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-03 10:29:09 +0530

    .

--- MinorProject/ColorMixer/src/App.jsx
+++ MinorProject/ColorMixer/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
 
   const colors =JSON.parse(localStorage.getItem('color'));
 
-  const [red,setRed] = useState(colors.r?colors.r:0);
-  const [green,setGreen] = useState(colors.g?colors.g:0);
-  const [blue,setBlue] = useState(colors.b?colors.b:0);
+  const [red,setRed] = useState(colors.red?colors.red:0);
+  const [green,setGreen] = useState(colors.green?colors.green:0);
+  const [blue,setBlue] = useState(colors.blue?colors.blue:0);
 
   
 
@@ -31,15 +31,15 @@ function App() {
           <tbody>
             <tr>
             <td><label htmlFor="red">Red : </label></td>
-            <td><input type="range" id='red' onChange={(e)=>setRed(e.target.value)} defaultValue={0} min={0} max={255}/></td>
+            <td><input type="range" id='red' value={red} onChange={(e)=>setRed(e.target.value)} min={0} max={255}/></td>
           </tr>
           <tr>
             <td><label htmlFor="green">Green : </label></td>
-            <td><input type="range" id='green' onChange={(e)=>setGreen(e.target.value)} defaultValue={0} min={0} max={255}/></td>
+            <td><input type="range" id='green' value={green} onChange={(e)=>setGreen(e.target.value)} min={0} max={255}/></td>
           </tr>
           <tr>
             <td><label htmlFor="blue">Blue : </label></td>
-            <td><input type="range" id='blue' onChange={(e)=>setBlue(e.target.value)} defaultValue={0} min={0} max={255}/></td>
+            <td><input type="range" id='blue' value={blue} onChange={(e)=>setBlue(e.target.value)} min={0} max={255}/></td>
           </tr>
           <tr>
             <td colSpan={2}>
