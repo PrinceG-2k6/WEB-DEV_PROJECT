60888ed17931614378095bce679b4f8fd9297d4f
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-03 09:54:10 +0530

    .

---
+++ MinorProject/ColorMixer/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [red,setRed] = useState(0);
+  const [green,setGreen] = useState(0);
+  const [blue,setBlue] = useState(0);
+
+  const handleRed =(e)={
+    
+  }
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
