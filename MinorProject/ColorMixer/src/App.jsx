249e24a83f43c5692dbc25ce10e399ea0af8d5a1
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-03 10:26:21 +0530

    .

--- MinorProject/ColorMixer/src/App.jsx
+++ MinorProject/ColorMixer/src/App.jsx
@@ -5,43 +5,48 @@ import './App.css'
 
 function App() {
 
-  const [red,setRed] = useState(0);
-  const [green,setGreen] = useState(0);
-  const [blue,setBlue] = useState(0);
+  const colors =JSON.parse(localStorage.getItem('color'));
 
-  const handleRed =(value)=>{
-    setRed(value)
-  }
-  const handleGreen =(value)=>{
-    setGreen(value)
-  }
-  const handleBlue =(value)=>{
-    setBlue(value)
+  const [red,setRed] = useState(colors.r?colors.r:0);
+  const [green,setGreen] = useState(colors.g?colors.g:0);
+  const [blue,setBlue] = useState(colors.b?colors.b:0);
+
+  
+
+  const save=()=>{
+    localStorage.setItem('color',JSON.stringify({red,green,blue}))
   }
 
   return (
     <>
       <div style={{
-        backgroundColor:'rgb(0,255,0)',
+        backgroundColor:'rgb('+red+','+green+','+blue+')',
         height:'500px',
         width:'500px'
       }} className="container">
 
       </div>
-      <form action="">
+      <form>
         <table>
-          <tr>
+          <tbody>
+            <tr>
             <td><label htmlFor="red">Red : </label></td>
-            <td><input type="range" id='red' defaultValue={0} min={0} max={255}/></td>
+            <td><input type="range" id='red' onChange={(e)=>setRed(e.target.value)} defaultValue={0} min={0} max={255}/></td>
           </tr>
           <tr>
             <td><label htmlFor="green">Green : </label></td>
-            <td><input type="range" id='green' defaultValue={0} min={0} max={255}/></td>
+            <td><input type="range" id='green' onChange={(e)=>setGreen(e.target.value)} defaultValue={0} min={0} max={255}/></td>
           </tr>
           <tr>
             <td><label htmlFor="blue">Blue : </label></td>
-            <td><input type="range" id='blue' defaultValue={0} min={0} max={255}/></td>
+            <td><input type="range" id='blue' onChange={(e)=>setBlue(e.target.value)} defaultValue={0} min={0} max={255}/></td>
           </tr>
+          <tr>
+            <td colSpan={2}>
+              <button onClick={save}>Save Combination</button>
+            </td>
+          </tr>
+          </tbody>
         </table>
       </form>
     </>
